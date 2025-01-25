a4efad836a28e95c8471b958101a3a2292dfcf1d
Nicole Oliver <nicole.oliver.42@gmail.com>
2025-01-24 16:46:34 -0800

    Watering can icon (#9)
    * create illustration page
    
    * copper watering can icon
    
    * move illustrations to /illustrations
    
    * adjust type scale
    
    * hide links

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('work');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/work/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("garden");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/garden/${post.slug}/`,
+    })),
+  });
 }
